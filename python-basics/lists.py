7b56e7aeb4199df51d3cf0ac40e8a9ce7906db69
yomi <whoisyomi@gmail.com>
2026-09-17 10:05:37 +0200

    Add lists and for loop examples

---
+++ python-basics/lists.py
@@ -0,0 +1,8 @@
+environments = ["DEX", "TEST", "UAT"]
+print(environments)
+print(environments[0])
+print(environments[1])
+print(environments[2])
+
+for environment in environments:
+    print(environment + " from loop")
